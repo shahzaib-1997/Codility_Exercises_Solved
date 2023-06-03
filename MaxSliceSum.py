adf832847c7d7b44a3ea1912ab28f8f010497335
shahzaib-1997 <smsashafqat@gmail.com7>
2023-06-04 02:34:40 +0500

    Added MaxSliceSum.py File

---
+++ MaxSliceSum.py
@@ -0,0 +1,40 @@
+'''
+A non-empty array A consisting of N integers is given. A pair of integers (P, Q), such that 0 ≤ P ≤ Q < N, is called a slice of array A. The sum of a slice (P, Q) is the total of A[P] + A[P+1] + ... + A[Q].
+
+Write a function:
+
+def solution(A)
+
+that, given an array A consisting of N integers, returns the maximum sum of any slice of A.
+
+For example, given array A such that:
+
+A[0] = 3  A[1] = 2  A[2] = -6
+A[3] = 4  A[4] = 0
+the function should return 5 because:
+
+(3, 4) is a slice of A that has sum 4,
+(2, 2) is a slice of A that has sum −6,
+(0, 1) is a slice of A that has sum 5,
+no other slice of A has sum greater than (0, 1).
+Write an efficient algorithm for the following assumptions:
+
+N is an integer within the range [1..1,000,000];
+each element of array A is an integer within the range [−1,000,000..1,000,000];
+the result will be an integer within the range [−2,147,483,648..2,147,483,647].
+'''
+
+def solution(A):
+    max_sum = -2147483649
+    val = 0
+
+    if len(A) > 1:
+        for a in A:
+            val = max(a,val + a)
+            max_sum = max(max_sum,val)
+    else:
+        max_sum = A[0]
+
+    return max_sum
+
+print(solution([3,2,-6,4,0]))
